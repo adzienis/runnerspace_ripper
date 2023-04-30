141c8d3ebba18bbc3c17e91a94b16287fd296f13
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2023-04-30 08:02:02 -0400

    formatted code

--- background.js
+++ background.js
@@ -1,23 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { 
-    const match = details.url.match(/(segment\d+\.ts)/g)
-    if(match) {
-      if(details.url.match(/fakefake=fakefake/g)) return null;
+  function (details) {
+    const match = details.url.match(/(segment\d+\.ts)/g);
+    if (match) {
+      if (details.url.match(/fakefake=fakefake/g)) return null;
 
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (!tabs[0]) return null;
 
-        if(!tabs[0]) return null;
-
-        chrome.tabs.sendMessage(
-          tabs[0].id, 
-          { url: details.url, 
-            segment_idx: match[0],
-              tab_url: tabs[0].url}
-            );
+        chrome.tabs.sendMessage(tabs[0].id, {
+          url: details.url,
+          segment_idx: match[0],
+          tab_url: tabs[0].url,
+        });
       });
-
-
     }
-    },
-  {urls: ["<all_urls>"]}
+  },
+  { urls: ["<all_urls>"] }
 );
--- scripts/content.js
+++ scripts/content.js
@@ -1,30 +1,26 @@
 const lut = {};
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if(lut[request.tab_url]) return null;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (lut[request.tab_url]) return null;
 
-      lut[request.tab_url] = true;
+  lut[request.tab_url] = true;
 
-      new Array(33).fill(null).map(async (_, i) => {
+  new Array(33).fill(null).map(async (_, i) => {
+    setTimeout(async () => {
+      const new_url = request.url.replace(/segment\d+\.ts/g, `segment${i}.ts`);
+      const resp = await fetch(`${new_url}&fakefake=fakefake`);
+      const blob = await resp.blob();
+      var file = window.URL.createObjectURL(blob);
+      const name = `segment${i.toString().padStart(2, "0")}.ts`;
 
-        setTimeout(async () => {
-            const new_url = request.url.replace(/segment\d+\.ts/g, `segment${i}.ts`)
-            const resp = await fetch(`${new_url}&fakefake=fakefake`)
-            const blob = await resp.blob()
-            var file = window.URL.createObjectURL(blob);
-            const name = `segment${i.toString().padStart(2, '0')}.ts`
+      var fileLink = document.createElement("a");
+      fileLink.href = file;
 
-            var fileLink = document.createElement('a');
-            fileLink.href = file;
+      // it forces the name of the downloaded file
+      fileLink.download = name;
 
-            // it forces the name of the downloaded file
-            fileLink.download = name;
-
-            // triggers the click event
-            fileLink.click();
-        }, i *500)
-      })
-    }
-  );
-
+      // triggers the click event
+      fileLink.click();
+    }, i * 500);
+  });
+});
